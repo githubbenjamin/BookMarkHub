05ff4113eb530e4ea481e89e5725921bb352f30d
githubbenjamin <yaoze189@yeah.net>
2017-07-11 19:22:52 +0800

    Update __init__.py

--- bookmark/api_v1/__init__.py
+++ bookmark/api_v1/__init__.py
@@ -1,7 +1,24 @@
 # coding=utf-8
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_restful import Api
 
-bp = Blueprint('apiv1', __name__, url_prefix='/api/v1')
+#bp = Blueprint('apiv1', __name__, url_prefix='/api/v1')
+bp=Blueprint('apiv1', __name__,
+             url_prefix='/api/v1',
+          template_folder='templates',
+          static_folder='static')
+
+@bp.route('/<user_url_slug>/hello')
+def hello(user_url_slug):
+    # Do some stuff
+    return render_template('hello.html', un=user_url_slug)
+
+#@bp.route('/<user_url_slug>/login')
+#def hello2(user_url_slug):
+#    # Do some stuff
+#    return render_template('login.html')
+
+from bp_login import *
+
 api = Api(bp)
