71a2829413ee9072aa3d643f348cd18359edd942
githubbenjamin <yaoze189@yeah.net>
2017-07-11 19:22:00 +0800

    add command list_rules

--- manage.py
+++ manage.py
@@ -3,14 +3,31 @@
 from bookmark.app import create_app
 from flask_script import Manager, Server
 
+
 app = create_app()
 manager = Manager(app)
+
+@manager.command
+def list_routes():
+    import urllib
+    
+    output = []
+    for rule in app.url_map.iter_rules():
+        methods = ','.join(rule.methods)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, rule))
+        output.append(line)
+    
+    for line in sorted(output):
+        print(line)
+
 manager.add_command('runserver', Server(
     use_reloader=True,
     use_debugger=True,
     host='127.0.0.1',
     port=5000
 ))
+
+
 
 if __name__ == '__main__':
     manager.run()
